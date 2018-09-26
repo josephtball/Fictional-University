9fe19a403137c69bc6a6774082ca51f4444c0d2b
Joseph T. Ball <jball321@live.com>
2018-09-26 17:56:54 -0400

    added dynamic blog posts to the home page (front-page.php) and updated blog link in header.php

--- front-page.php
+++ front-page.php
@@ -46,29 +46,39 @@
     <div class="full-width-split__two">
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+            <?php
+                // this line of code is setting up a custom query using a WP class used for quering
+                $homepagePosts = new WP_Query(array(
+                    'posts_per_page' => 2
+                ));
 
-            <div class="event-summary">
-                <a class="event-summary__date event-summary__date--beige t-center" href="#">
-                    <span class="event-summary__month">Jan</span>
-                    <span class="event-summary__day">20</span>  
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
-                    <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
-                </div>
-            </div>
-            <div class="event-summary">
-                <a class="event-summary__date event-summary__date--beige t-center" href="#">
-                    <span class="event-summary__month">Feb</span>
-                    <span class="event-summary__day">04</span>  
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
-                    <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
-                </div>
-            </div>
+                while ($homepagePosts->have_posts()) {
+                    $homepagePosts->the_post(); ?>
+                    
+                    <div class="event-summary">
+                        <!-- 'the_permalink()' outputs a link. In this case to the post -->
+                        <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                            <!-- 'the_time()' outputs the date and time in different formats of a post -->
+                            <span class="event-summary__month"><?php the_time('M'); ?></span>
+                            <span class="event-summary__day"><?php the_time('d'); ?></span>  
+                        </a>
+                        <div class="event-summary__content">
+                            <!-- 'the_permalink()' outputs a link. In this case to the post -->
+                            <!-- 'the_title()' outputs the title of a post -->
+                            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                            <!-- 'wp_trim_words()' shortens selected content to a designated amount of words -->
+                            <!-- 'the_permalink()' outputs a link. In this case to the post -->
+                            <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                        </div>
+                    </div>
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+                <?php
+                // 'wp_reset_postdata()' is a function that resets WP data and global variables back to the default state
+                } wp_reset_postdata();
+            ?>
+
+            <!-- 'echo site_url()' outputs the root site url -->
+            <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
     </div>
 </div>
--- header.php
+++ header.php
@@ -32,7 +32,9 @@
                             <li><a href="#">Programs</a></li>
                             <li><a href="#">Events</a></li>
                             <li><a href="#">Campuses</a></li>
-                            <li><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
+                            <!-- if statement checks if the current page has a post type of "post". If true gives class attribute to <li> -->
+                            <!-- 'echo site_url()' outputs the root site url -->
+                            <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
                         </ul>
                     </nav>
                     <div class="site-header__util">
