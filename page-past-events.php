10d43deaae60058ef765f7bd66d3e45cd37f9b01
Joseph T. Ball <jball321@live.com>
2018-10-22 23:50:43 -0400

    added past events page. 'page-past-events.php

--- page-past-events.php
+++ page-past-events.php
@@ -10,9 +10,9 @@
     <!-- 'get_theme_file_uri()' oupouts the location of the theme folder -->
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>);"></div>
     <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">All Events</h1>
+    <h1 class="page-banner__title">Past Events</h1>
     <div class="page-banner__intro">
-        <p>See what is going on in our world.</p>
+        <p>A recap of our past events.</p>
     </div>
     </div>  
 </div>
@@ -20,10 +20,29 @@
 <div class="container container--narrow page-section">
 
 <?php
+    $today = date('Ymd');
+
+    // this line of code is setting up a custom query using a WP class used for quering
+    $pastEvents = new WP_Query(array(
+        'paged' => get_query_var('paged', 1),
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',        
+        'order' => 'ASC',
+        'meta_query' => array(
+            array(
+                'key' => 'event_date',
+                'compare' => '<',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+        )
+    ));
+
     // loops through posts
-    while (have_posts()) {
+    while ($pastEvents->have_posts()) {
         // 'the_post()' is used to iterate the post index in the loop
-        the_post(); ?>
+        $pastEvents->the_post(); ?>
 
         <div class="event-summary">
             <!-- 'the_permalink()' outputs a link. In this case to the post -->
@@ -49,11 +68,10 @@
     }
 
     // this function retrieves paginated links for archived posts
-    echo paginate_links();
+    echo paginate_links(array(
+        'total' => $pastEvents->max_num_pages
+    ));
 ?>
-
-<hr class='section-break'>
-<p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events') ?>">Check out our past events archive</a>.</p>
 
 </div>
 
